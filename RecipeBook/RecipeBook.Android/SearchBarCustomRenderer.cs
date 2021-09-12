11e2c213872e2083a8a74c61b759ef4235d7925a
Bartosz Ogrodnik <bartosz_ogrodnik@outlook.com>
2021-09-12 19:13:12 +0200

    workday

---
+++ RecipeBook/RecipeBook.Android/SearchBarCustomRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using RecipeBook.CustomRenders;
+using RecipeBook.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(SearchBarCustom), typeof(SearchBarCustomRenderer))]
+namespace RecipeBook.Droid
+{
+    public class SearchBarCustomRenderer : SearchBarRenderer
+    {
+        public SearchBarCustomRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                var searchView = Control;
+                searchView.Iconified = true;
+                searchView.SetIconifiedByDefault(false);
+                int searchIconId = Context.Resources.GetIdentifier("android:id/search_mag_icon", null, null);
+                var icon = searchView.FindViewById(searchIconId);
+                (icon as ImageView).SetImageResource(Resource.Drawable.search_300_wb);
+                
+
+                var plateId = Resources.GetIdentifier("android:id/search_plate", null, null);
+                var plate = Control.FindViewById(plateId);
+                plate.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
---
+++ RecipeBook/RecipeBook/CustomRenders/SearchBarCustom.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RecipeBook.CustomRenders
+{
+    public class SearchBarCustom : SearchBar
+    {
+    }
+}
